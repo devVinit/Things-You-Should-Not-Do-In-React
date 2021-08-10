955370f061d637103b49cb842714c70f26b36d25
devVinit <dev.vinit18@gmail.com>
2021-08-11 01:02:25 +0530

    Case2: correct way to use memo in react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import Case1 from './cases/case1/case1';
+// import Case1 from './cases/case1/case1';
+import Case2 from './cases/case2/case2';
 
 function App() {
   return (
     <div className="App">
-      <Case1 />
+      {/* <Case1 /> */}
+      <Case2 />
     </div>
   );
 }
